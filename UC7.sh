961508413b97528c043f298b0e197bbbc4854afa
unknown <nilimawadal1@gmail.com>
2021-03-16 00:30:20 +0530

    Refactor the code function

---
+++ UC7.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo welcome to the employee wage computation
+wageperHour=20
+dailyHours=8
+fullTime=1
+partTime=2
+workDay=20
+maxHours=100
+totalEmpHours=0
+days=1
+function hours()
+{
+case $empCheck in
+$fullTime)
+dailyHours=8
+;;
+$partTime)
+dailyHours=4
+;;
+*)
+dailyHours=0
+;;
+esac
+
+}
+while [ $totalEmpHours -lt $maxHours -a $days -le $workDay ]
+do 
+empCheck=$(( RANDOM%3 ))
+hours $empCheck
+totalEmpHours=$(($totalEmpHours+$dailyHour))
+
+empwagepermonth=$(($totalEmpHours*$workDay))
+echo “ Employee Wage for month : Rs. $empwagepermonth ”
+done
+
